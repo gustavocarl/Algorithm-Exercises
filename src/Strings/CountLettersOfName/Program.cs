3a725f63679d82afd08a2b613b2e9a8f3119f801
Gustavo Henrique Carl Dos Santos <gustavo_carl@live.com>
2022-07-23 00:31:51 -0300

    Added the Exercise to Check the Total Letters of a Name

---
+++ src/Strings/CountLettersOfName/Program.cs
@@ -0,0 +1,23 @@
+﻿int numberOfVowels = 0, numberOfConsonants = 0;
+
+Console.WriteLine("Informe um nome");
+string? completeName = Console.ReadLine();
+
+completeName = completeName?.ToUpper();
+completeName = completeName?.Replace(" ", "");
+
+for (var i = 0; i < completeName?.Length; i++)
+{
+    if ((completeName[i] == 'A') ||
+        (completeName[i] == 'E') ||
+        (completeName[i] == 'I') ||
+        (completeName[i] == 'O') ||
+        (completeName[i] == 'U'))
+        numberOfVowels++;
+    else
+        numberOfConsonants++;
+}
+
+Console.WriteLine($"O nome digitado tem: {numberOfVowels} vogais");
+Console.WriteLine($"O nome digitado tem: {numberOfConsonants} consoantes");
+Console.WriteLine($"O nome digitado tem: {numberOfVowels + numberOfConsonants} letras");
